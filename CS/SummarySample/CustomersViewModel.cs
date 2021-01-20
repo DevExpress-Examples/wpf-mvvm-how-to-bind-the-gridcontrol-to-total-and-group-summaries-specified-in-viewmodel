8527dbedfd19b22caf6a77e0117775af1615dcb7
AndreySlabov <65009440+AndreySlabov@users.noreply.github.com>
2021-01-20 17:01:33 +0300

    Add files via upload

---
+++ CS/SummarySample/CustomersViewModel.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.Mvvm;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SummarySample {
+    public enum SummaryItemType { Max, Count, None }
+
+    public class CustomersViewModel : ViewModelBase {
+        public CustomersViewModel() {
+            Customers = CustomersDataModel.GetCustomers();
+            TotalSummary = new ObservableCollection<Summary>() {
+                new Summary( SummaryItemType.Count, nameof(Customer.Name) ),
+                new Summary(SummaryItemType.Max, nameof(Customer.Visits) )
+            };
+            GroupSummary = new ObservableCollection<Summary>() {
+                new Summary( SummaryItemType.Count, nameof(Customer.Name) )
+            };
+        }
+        public IList<Customer> Customers { get; }
+        public ObservableCollection<Summary> TotalSummary { get; }
+        public ObservableCollection<Summary> GroupSummary { get; }
+    }
+
+    public class Summary : BindableBase {
+        public Summary(SummaryItemType type, string fieldname) {
+            Type = type;
+            FieldName = fieldname;
+        }
+        public SummaryItemType Type { get; }
+        public string FieldName { get; }
+    }
+}
